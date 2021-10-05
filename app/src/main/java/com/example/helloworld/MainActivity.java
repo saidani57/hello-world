1419a919e7be8a7b345d910d0656db34042de837
Anis <saidanianis763@gmail.com>
2021-10-05 02:16:47 +0100

    success

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.helloworld;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "Hello World");
+        Log.i("MainActivity", "MainActivity layout is complete");
+
+    }
+}
